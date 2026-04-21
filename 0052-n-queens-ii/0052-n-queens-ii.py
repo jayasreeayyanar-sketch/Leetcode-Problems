8a58a270e70d16627c98b22b722dd43e6c9db21e
jayasreeayyanar-sketch <jayasreeayyanar@gmail.com>
2026-04-21 20:46:09 +0530

    Time: 9 ms (72.98%), Space: 12.3 MB (65.47%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def totalNQueens(self, n):
+        cols = set()
+        diag1 = set()  
+        diag2 = set()  
+        def backtrack(row):
+            if row == n:
+                return 1 
+            count = 0            
+            for col in range(n):
+                if col in cols or (row - col) in diag1 or (row + col) in diag2:
+                    continue
+                cols.add(col)
+                diag1.add(row - col)
+                diag2.add(row + col)                
+                count += backtrack(row + 1)
+                cols.remove(col)
+                diag1.remove(row - col)
+                diag2.remove(row + col)            
+            return count        
+        return backtrack(0)
