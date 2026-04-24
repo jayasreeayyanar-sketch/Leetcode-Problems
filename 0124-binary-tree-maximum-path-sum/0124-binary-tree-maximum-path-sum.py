8a265f11eb47e714bbb97b3881b38f1f1a843681
jayasreeayyanar-sketch <jayasreeayyanar@gmail.com>
2026-04-24 14:44:51 +0530

    Time: 22 ms (79.64%), Space: 25.5 MB (82.91%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxPathSum(self, root):
+        self.max_sum = float('-inf')        
+        def get_max_gain(node):
+            if not node:
+                return 0
+            left_gain = max(get_max_gain(node.left), 0)
+            right_gain = max(get_max_gain(node.right), 0)
+            current_path_sum = node.val + left_gain + right_gain
+            self.max_sum = max(self.max_sum, current_path_sum)
+            return node.val + max(left_gain, right_gain)        
+        get_max_gain(root)
+        return self.max_sum
