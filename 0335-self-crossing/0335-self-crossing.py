61731736a09fb738540ebbf97ac09b2829221269
jayasreeayyanar-sketch <jayasreeayyanar@gmail.com>
2026-08-05 21:15:51 +0530

    Time: 43 ms (37.13%), Space: 19.7 MB (40.12%) - LeetHub

---
+++ 0335-self-crossing/0335-self-crossing.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isSelfCrossing(self, distance):
+        n = len(distance)
+        for i in range(3, n):
+            if distance[i] >= distance[i - 2] and \
+               distance[i - 1] <= distance[i - 3]:
+                return True
+            if i >= 4:
+                if distance[i - 1] == distance[i - 3] and \
+                   distance[i] + distance[i - 4] >= distance[i - 2]:
+                    return True
+            if i >= 5:
+                if distance[i - 2] >= distance[i - 4] and \
+                   distance[i] + distance[i - 4] >= distance[i - 2] and \
+                   distance[i - 1] <= distance[i - 3] and \
+                   distance[i - 1] + distance[i - 5] >= distance[i - 3]:
+                    return True
+        return False
